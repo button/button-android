84ecad6030648485b3fc43ef029526d7858d0fdc
Build Server <buildserver@Builds-Mac-mini.local>
2015-06-11 12:37:11 -0400

    Added JavaDoc for 0.9.13

---
+++ latest/assets/navtree_data.js
@@ -0,0 +1,13 @@
+var NAVTREE_DATA =
+[ [ "com.usebutton.sdk", "com/usebutton/sdk/package-summary.html", [ [ "Interfaces", null, [ [ "ButtonDropin.Listener", "com/usebutton/sdk/ButtonDropin.Listener.html", null, "" ], [ "ButtonDropin.OnDropinEvents", "com/usebutton/sdk/ButtonDropin.OnDropinEvents.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "Button", "com/usebutton/sdk/Button.html", null, "" ], [ "ButtonDropin", "com/usebutton/sdk/ButtonDropin.html", null, "" ], [ "PlacementContext", "com/usebutton/sdk/PlacementContext.html", null, "" ], [ "PlacementContext.Location", "com/usebutton/sdk/PlacementContext.Location.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.usebutton.sdk.activity", "com/usebutton/sdk/activity/package-summary.html", [ [ "Classes", null, [ [ "PromotionTestActivity", "com/usebutton/sdk/activity/PromotionTestActivity.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.usebutton.sdk.util", "com/usebutton/sdk/util/package-summary.html", [ [ "Interfaces", null, [ [ "Receiver", "com/usebutton/sdk/util/Receiver.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "Arrays", "com/usebutton/sdk/util/Arrays.html", null, "" ], [ "IdentifierForAdvertiserProvider", "com/usebutton/sdk/util/IdentifierForAdvertiserProvider.html", null, "" ], [ "LatLngPair", "com/usebutton/sdk/util/LatLngPair.html", null, "" ], [ "LocationProvider", "com/usebutton/sdk/util/LocationProvider.html", null, "" ], [ "PackageObserver", "com/usebutton/sdk/util/PackageObserver.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
