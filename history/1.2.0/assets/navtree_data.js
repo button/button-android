89733e06b52fe3a9700248fddd811d52c4e5a10c
Sveinung Kval Bakken <sveinung.bakken@gmail.com>
2015-07-10 13:47:16 -0400

    Added 1.2.0 docs to history

---
+++ history/1.2.0/assets/navtree_data.js
@@ -0,0 +1,11 @@
+var NAVTREE_DATA =
+[ [ "com.usebutton.sdk", "com/usebutton/sdk/package-summary.html", [ [ "Interfaces", null, [ [ "ButtonDropin.Listener", "com/usebutton/sdk/ButtonDropin.Listener.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "Button", "com/usebutton/sdk/Button.html", null, "" ], [ "ButtonDropin", "com/usebutton/sdk/ButtonDropin.html", null, "" ], [ "PlacementContext", "com/usebutton/sdk/PlacementContext.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.usebutton.sdk.util", "com/usebutton/sdk/util/package-summary.html", [ [ "Interfaces", null, [ [ "Receiver", "com/usebutton/sdk/util/Receiver.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "Arrays", "com/usebutton/sdk/util/Arrays.html", null, "" ], [ "LatLngPair", "com/usebutton/sdk/util/LatLngPair.html", null, "" ], [ "LocationProvider", "com/usebutton/sdk/util/LocationProvider.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ history/1.2.0/reference/lists.js
@@ -0,0 +1,13 @@
+var DATA = [
+      { id:0, label:"com.usebutton.sdk", link:"com/usebutton/sdk/package-summary.html", type:"package" },
+      { id:1, label:"com.usebutton.sdk.Button", link:"com/usebutton/sdk/Button.html", type:"class" },
+      { id:2, label:"com.usebutton.sdk.ButtonDropin", link:"com/usebutton/sdk/ButtonDropin.html", type:"class" },
+      { id:3, label:"com.usebutton.sdk.ButtonDropin.Listener", link:"com/usebutton/sdk/ButtonDropin.Listener.html", type:"class" },
+      { id:4, label:"com.usebutton.sdk.PlacementContext", link:"com/usebutton/sdk/PlacementContext.html", type:"class" },
+      { id:5, label:"com.usebutton.sdk.util", link:"com/usebutton/sdk/util/package-summary.html", type:"package" },
+      { id:6, label:"com.usebutton.sdk.util.Arrays", link:"com/usebutton/sdk/util/Arrays.html", type:"class" },
+      { id:7, label:"com.usebutton.sdk.util.LatLngPair", link:"com/usebutton/sdk/util/LatLngPair.html", type:"class" },
+      { id:8, label:"com.usebutton.sdk.util.LocationProvider", link:"com/usebutton/sdk/util/LocationProvider.html", type:"class" },
+      { id:9, label:"com.usebutton.sdk.util.Receiver", link:"com/usebutton/sdk/util/Receiver.html", type:"class" }
+
+    ];
