2c30ab1f2b548100ad92884cec66c0db38bd38b4
Build Server <buildserver@Builds-Mac-mini.local>
2015-07-27 16:49:32 -0400

    Added JavaDoc for

--- history/assets/navtree_data.js
+++ history/assets/navtree_data.js
@@ -3,7 +3,7 @@ var NAVTREE_DATA =
 , "" ], [ "Classes", null, [ [ "Button", "com/usebutton/sdk/Button.html", null, "" ], [ "ButtonDropin", "com/usebutton/sdk/ButtonDropin.html", null, "" ], [ "PlacementContext", "com/usebutton/sdk/PlacementContext.html", null, "" ] ]
 , "" ] ]
 , "" ], [ "com.usebutton.sdk.util", "com/usebutton/sdk/util/package-summary.html", [ [ "Interfaces", null, [ [ "Receiver", "com/usebutton/sdk/util/Receiver.html", null, "" ] ]
-, "" ], [ "Classes", null, [ [ "Arrays", "com/usebutton/sdk/util/Arrays.html", null, "" ], [ "LatLngPair", "com/usebutton/sdk/util/LatLngPair.html", null, "" ], [ "LocationProvider", "com/usebutton/sdk/util/LocationProvider.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "Arrays", "com/usebutton/sdk/util/Arrays.html", null, "" ], [ "CompatHelpers", "com/usebutton/sdk/util/CompatHelpers.html", null, "" ], [ "CompatHelpers.RippleBackground", "com/usebutton/sdk/util/CompatHelpers.RippleBackground.html", null, "" ], [ "LatLngPair", "com/usebutton/sdk/util/LatLngPair.html", null, "" ], [ "LocationProvider", "com/usebutton/sdk/util/LocationProvider.html", null, "" ] ]
 , "" ] ]
 , "" ] ]
 
--- history/reference/lists.js
+++ history/reference/lists.js
@@ -6,8 +6,10 @@ var DATA = [
       { id:4, label:"com.usebutton.sdk.PlacementContext", link:"com/usebutton/sdk/PlacementContext.html", type:"class" },
       { id:5, label:"com.usebutton.sdk.util", link:"com/usebutton/sdk/util/package-summary.html", type:"package" },
       { id:6, label:"com.usebutton.sdk.util.Arrays", link:"com/usebutton/sdk/util/Arrays.html", type:"class" },
-      { id:7, label:"com.usebutton.sdk.util.LatLngPair", link:"com/usebutton/sdk/util/LatLngPair.html", type:"class" },
-      { id:8, label:"com.usebutton.sdk.util.LocationProvider", link:"com/usebutton/sdk/util/LocationProvider.html", type:"class" },
-      { id:9, label:"com.usebutton.sdk.util.Receiver", link:"com/usebutton/sdk/util/Receiver.html", type:"class" }
+      { id:7, label:"com.usebutton.sdk.util.CompatHelpers", link:"com/usebutton/sdk/util/CompatHelpers.html", type:"class" },
+      { id:8, label:"com.usebutton.sdk.util.CompatHelpers.RippleBackground", link:"com/usebutton/sdk/util/CompatHelpers.RippleBackground.html", type:"class" },
+      { id:9, label:"com.usebutton.sdk.util.LatLngPair", link:"com/usebutton/sdk/util/LatLngPair.html", type:"class" },
+      { id:10, label:"com.usebutton.sdk.util.LocationProvider", link:"com/usebutton/sdk/util/LocationProvider.html", type:"class" },
+      { id:11, label:"com.usebutton.sdk.util.Receiver", link:"com/usebutton/sdk/util/Receiver.html", type:"class" }
 
     ];
