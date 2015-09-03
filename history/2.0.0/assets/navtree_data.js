40e01d83126185ea1be1fe9f4f546c27befeebf4
Build Server <buildserver@Builds-Mac-mini.local>
2015-09-03 16:24:21 -0400

    Added JavaDoc for 2.0.0

---
+++ history/2.0.0/assets/navtree_data.js
@@ -0,0 +1,13 @@
+var NAVTREE_DATA =
+[ [ "android.support.v4.view", "android/support/v4/view/package-summary.html", [ [ "Classes", null, [ [ "PagerTabStripV22", "android/support/v4/view/PagerTabStripV22.html", null, "" ], [ "PagerTitleStripV22", "android/support/v4/view/PagerTitleStripV22.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.usebutton.sdk", "com/usebutton/sdk/package-summary.html", [ [ "Interfaces", null, [ [ "ButtonDropin.Listener", "com/usebutton/sdk/ButtonDropin.Listener.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "Button", "com/usebutton/sdk/Button.html", null, "" ], [ "ButtonDropin", "com/usebutton/sdk/ButtonDropin.html", null, "" ], [ "PlacementContext", "com/usebutton/sdk/PlacementContext.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.usebutton.sdk.util", "com/usebutton/sdk/util/package-summary.html", [ [ "Interfaces", null, [ [ "Receiver", "com/usebutton/sdk/util/Receiver.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "Arrays", "com/usebutton/sdk/util/Arrays.html", null, "" ], [ "CompatHelpers", "com/usebutton/sdk/util/CompatHelpers.html", null, "" ], [ "CompatHelpers.RippleBackground", "com/usebutton/sdk/util/CompatHelpers.RippleBackground.html", null, "" ], [ "LocationProvider", "com/usebutton/sdk/util/LocationProvider.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
