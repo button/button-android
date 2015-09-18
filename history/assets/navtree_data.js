5ef692f0595e4fda82e71732e2c2883d293517c5
Build Server <buildserver@Builds-Mac-mini.local>
2015-09-18 13:14:41 -0400

    Added JavaDoc for

--- history/assets/navtree_data.js
+++ history/assets/navtree_data.js
@@ -1,7 +1,5 @@
 var NAVTREE_DATA =
-[ [ "android.support.v4.view", "android/support/v4/view/package-summary.html", [ [ "Classes", null, [ [ "PagerTabStripV22", "android/support/v4/view/PagerTabStripV22.html", null, "" ], [ "PagerTitleStripV22", "android/support/v4/view/PagerTitleStripV22.html", null, "" ] ]
-, "" ] ]
-, "" ], [ "com.usebutton.sdk", "com/usebutton/sdk/package-summary.html", [ [ "Interfaces", null, [ [ "ButtonDropin.Listener", "com/usebutton/sdk/ButtonDropin.Listener.html", null, "" ] ]
+[ [ "com.usebutton.sdk", "com/usebutton/sdk/package-summary.html", [ [ "Interfaces", null, [ [ "ButtonDropin.Listener", "com/usebutton/sdk/ButtonDropin.Listener.html", null, "" ] ]
 , "" ], [ "Classes", null, [ [ "Button", "com/usebutton/sdk/Button.html", null, "" ], [ "ButtonDropin", "com/usebutton/sdk/ButtonDropin.html", null, "" ], [ "PlacementContext", "com/usebutton/sdk/PlacementContext.html", null, "" ] ]
 , "" ] ]
 , "" ], [ "com.usebutton.sdk.util", "com/usebutton/sdk/util/package-summary.html", [ [ "Interfaces", null, [ [ "Receiver", "com/usebutton/sdk/util/Receiver.html", null, "" ] ]
--- history/reference/lists.js
+++ history/reference/lists.js
@@ -1,17 +1,14 @@
 var DATA = [
-      { id:0, label:"android.support.v4.view", link:"android/support/v4/view/package-summary.html", type:"package" },
-      { id:1, label:"android.support.v4.view.PagerTabStripV22", link:"android/support/v4/view/PagerTabStripV22.html", type:"class" },
-      { id:2, label:"android.support.v4.view.PagerTitleStripV22", link:"android/support/v4/view/PagerTitleStripV22.html", type:"class" },
-      { id:3, label:"com.usebutton.sdk", link:"com/usebutton/sdk/package-summary.html", type:"package" },
-      { id:4, label:"com.usebutton.sdk.Button", link:"com/usebutton/sdk/Button.html", type:"class" },
-      { id:5, label:"com.usebutton.sdk.ButtonDropin", link:"com/usebutton/sdk/ButtonDropin.html", type:"class" },
-      { id:6, label:"com.usebutton.sdk.ButtonDropin.Listener", link:"com/usebutton/sdk/ButtonDropin.Listener.html", type:"class" },
-      { id:7, label:"com.usebutton.sdk.PlacementContext", link:"com/usebutton/sdk/PlacementContext.html", type:"class" },
-      { id:8, label:"com.usebutton.sdk.util", link:"com/usebutton/sdk/util/package-summary.html", type:"package" },
-      { id:9, label:"com.usebutton.sdk.util.Arrays", link:"com/usebutton/sdk/util/Arrays.html", type:"class" },
-      { id:10, label:"com.usebutton.sdk.util.CompatHelpers", link:"com/usebutton/sdk/util/CompatHelpers.html", type:"class" },
-      { id:11, label:"com.usebutton.sdk.util.CompatHelpers.RippleBackground", link:"com/usebutton/sdk/util/CompatHelpers.RippleBackground.html", type:"class" },
-      { id:12, label:"com.usebutton.sdk.util.LocationProvider", link:"com/usebutton/sdk/util/LocationProvider.html", type:"class" },
-      { id:13, label:"com.usebutton.sdk.util.Receiver", link:"com/usebutton/sdk/util/Receiver.html", type:"class" }
+      { id:0, label:"com.usebutton.sdk", link:"com/usebutton/sdk/package-summary.html", type:"package" },
+      { id:1, label:"com.usebutton.sdk.Button", link:"com/usebutton/sdk/Button.html", type:"class" },
+      { id:2, label:"com.usebutton.sdk.ButtonDropin", link:"com/usebutton/sdk/ButtonDropin.html", type:"class" },
+      { id:3, label:"com.usebutton.sdk.ButtonDropin.Listener", link:"com/usebutton/sdk/ButtonDropin.Listener.html", type:"class" },
+      { id:4, label:"com.usebutton.sdk.PlacementContext", link:"com/usebutton/sdk/PlacementContext.html", type:"class" },
+      { id:5, label:"com.usebutton.sdk.util", link:"com/usebutton/sdk/util/package-summary.html", type:"package" },
+      { id:6, label:"com.usebutton.sdk.util.Arrays", link:"com/usebutton/sdk/util/Arrays.html", type:"class" },
+      { id:7, label:"com.usebutton.sdk.util.CompatHelpers", link:"com/usebutton/sdk/util/CompatHelpers.html", type:"class" },
+      { id:8, label:"com.usebutton.sdk.util.CompatHelpers.RippleBackground", link:"com/usebutton/sdk/util/CompatHelpers.RippleBackground.html", type:"class" },
+      { id:9, label:"com.usebutton.sdk.util.LocationProvider", link:"com/usebutton/sdk/util/LocationProvider.html", type:"class" },
+      { id:10, label:"com.usebutton.sdk.util.Receiver", link:"com/usebutton/sdk/util/Receiver.html", type:"class" }
 
     ];
