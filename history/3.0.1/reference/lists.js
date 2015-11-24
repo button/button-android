2503cf40e8a73159c83937eee99b6312752abdb0
Build Server <buildserver@Builds-Mac-mini.local>
2015-11-24 11:24:45 -0500

    Added JavaDoc for 3.0.1

---
+++ history/3.0.1/reference/lists.js
@@ -0,0 +1,23 @@
+var DATA = [
+      { id:0, label:"com.usebutton.sdk", link:"com/usebutton/sdk/package-summary.html", type:"package" },
+      { id:1, label:"com.usebutton.sdk.Button", link:"com/usebutton/sdk/Button.html", type:"class" },
+      { id:2, label:"com.usebutton.sdk.ButtonContext", link:"com/usebutton/sdk/ButtonContext.html", type:"class" },
+      { id:3, label:"com.usebutton.sdk.ButtonDropin", link:"com/usebutton/sdk/ButtonDropin.html", type:"class" },
+      { id:4, label:"com.usebutton.sdk.ButtonDropin.Listener", link:"com/usebutton/sdk/ButtonDropin.Listener.html", type:"class" },
+      { id:5, label:"com.usebutton.sdk.context", link:"com/usebutton/sdk/context/package-summary.html", type:"package" },
+      { id:6, label:"com.usebutton.sdk.context.Event", link:"com/usebutton/sdk/context/Event.html", type:"class" },
+      { id:7, label:"com.usebutton.sdk.context.Identifiers", link:"com/usebutton/sdk/context/Identifiers.html", type:"class" },
+      { id:8, label:"com.usebutton.sdk.context.Identifiers.IdentifierName", link:"com/usebutton/sdk/context/Identifiers.IdentifierName.html", type:"class" },
+      { id:9, label:"com.usebutton.sdk.context.Item", link:"com/usebutton/sdk/context/Item.html", type:"class" },
+      { id:10, label:"com.usebutton.sdk.context.Journey", link:"com/usebutton/sdk/context/Journey.html", type:"class" },
+      { id:11, label:"com.usebutton.sdk.context.Journey.Type", link:"com/usebutton/sdk/context/Journey.Type.html", type:"class" },
+      { id:12, label:"com.usebutton.sdk.context.Location", link:"com/usebutton/sdk/context/Location.html", type:"class" },
+      { id:13, label:"com.usebutton.sdk.context.MusicArtist", link:"com/usebutton/sdk/context/MusicArtist.html", type:"class" },
+      { id:14, label:"com.usebutton.sdk.util", link:"com/usebutton/sdk/util/package-summary.html", type:"package" },
+      { id:15, label:"com.usebutton.sdk.util.Arrays", link:"com/usebutton/sdk/util/Arrays.html", type:"class" },
+      { id:16, label:"com.usebutton.sdk.util.CompatHelpers", link:"com/usebutton/sdk/util/CompatHelpers.html", type:"class" },
+      { id:17, label:"com.usebutton.sdk.util.CompatHelpers.RippleBackground", link:"com/usebutton/sdk/util/CompatHelpers.RippleBackground.html", type:"class" },
+      { id:18, label:"com.usebutton.sdk.util.LocationProvider", link:"com/usebutton/sdk/util/LocationProvider.html", type:"class" },
+      { id:19, label:"com.usebutton.sdk.util.Receiver", link:"com/usebutton/sdk/util/Receiver.html", type:"class" }
+
+    ];
