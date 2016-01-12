ccb2cadf3221dfd40a7d6934d85f83f8c83637ae
Build Server <buildserver@Builds-Mac-mini.local>
2016-01-12 18:15:21 -0500

    Added JavaDoc for 3.2.0

---
+++ history/3.2.0/assets/navtree_data.js
@@ -0,0 +1,15 @@
+var NAVTREE_DATA =
+[ [ "com.usebutton.sdk", "com/usebutton/sdk/package-summary.html", [ [ "Interfaces", null, [ [ "ButtonDropin.Listener", "com/usebutton/sdk/ButtonDropin.Listener.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "Button", "com/usebutton/sdk/Button.html", null, "" ], [ "ButtonContext", "com/usebutton/sdk/ButtonContext.html", null, "" ], [ "ButtonDropin", "com/usebutton/sdk/ButtonDropin.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.usebutton.sdk.context", "com/usebutton/sdk/context/package-summary.html", [ [ "Classes", null, [ [ "Event", "com/usebutton/sdk/context/Event.html", null, "" ], [ "Identifiers", "com/usebutton/sdk/context/Identifiers.html", null, "" ], [ "Item", "com/usebutton/sdk/context/Item.html", null, "" ], [ "Journey", "com/usebutton/sdk/context/Journey.html", null, "" ], [ "Location", "com/usebutton/sdk/context/Location.html", null, "" ], [ "MusicArtist", "com/usebutton/sdk/context/MusicArtist.html", null, "" ] ]
+, "" ], [ "Annotations", null, [ [ "Identifiers.IdentifierName", "com/usebutton/sdk/context/Identifiers.IdentifierName.html", null, "" ] ]
+, "" ], [ "Enums", null, [ [ "Journey.Type", "com/usebutton/sdk/context/Journey.Type.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.usebutton.sdk.util", "com/usebutton/sdk/util/package-summary.html", [ [ "Interfaces", null, [ [ "Receiver", "com/usebutton/sdk/util/Receiver.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "Arrays", "com/usebutton/sdk/util/Arrays.html", null, "" ], [ "CompatHelpers", "com/usebutton/sdk/util/CompatHelpers.html", null, "" ], [ "CompatHelpers.RippleBackground", "com/usebutton/sdk/util/CompatHelpers.RippleBackground.html", null, "" ], [ "LocationProvider", "com/usebutton/sdk/util/LocationProvider.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
