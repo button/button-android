f3b5a7be252eb6dc322698e2ca0b55a1ba676703
Build Agent 1 <build-agent-1@build-agent-1.local>
2018-04-03 21:48:33 -0400

    Added JavaDoc for 5.28.0

---
+++ history/5.28.0/assets/navtree_data.js
@@ -0,0 +1,23 @@
+var NAVTREE_DATA =
+[ [ "com.usebutton.sdk", "com/usebutton/sdk/package-summary.html", [ [ "Interfaces", null, [ [ "Button.ActionListener", "com/usebutton/sdk/Button.ActionListener.html", null, "" ], [ "Button.DeepLinkListener", "com/usebutton/sdk/Button.DeepLinkListener.html", null, "" ], [ "ButtonDropin.Listener", "com/usebutton/sdk/ButtonDropin.Listener.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "Button", "com/usebutton/sdk/Button.html", null, "" ], [ "ButtonContext", "com/usebutton/sdk/ButtonContext.html", null, "" ], [ "ButtonDropin", "com/usebutton/sdk/ButtonDropin.html", null, "" ], [ "InteractiveButton", "com/usebutton/sdk/InteractiveButton.html", null, "" ], [ "LineItem", "com/usebutton/sdk/LineItem.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.usebutton.sdk.checkout", "com/usebutton/sdk/checkout/package-summary.html", [ [ "Interfaces", null, [ [ "CheckoutExtension", "com/usebutton/sdk/checkout/CheckoutExtension.html", null, "" ], [ "CheckoutInterface", "com/usebutton/sdk/checkout/CheckoutInterface.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "CallToAction", "com/usebutton/sdk/checkout/CallToAction.html", null, "" ], [ "Card", "com/usebutton/sdk/checkout/Card.html", null, "" ], [ "Commission", "com/usebutton/sdk/checkout/Commission.html", null, "" ], [ "Product", "com/usebutton/sdk/checkout/Product.html", null, "" ], [ "TextCard", "com/usebutton/sdk/checkout/TextCard.html", null, "" ], [ "TextCard.Builder", "com/usebutton/sdk/checkout/TextCard.Builder.html", null, "" ] ]
+, "" ], [ "Enums", null, [ [ "Commission.CommissionType", "com/usebutton/sdk/checkout/Commission.CommissionType.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.usebutton.sdk.context", "com/usebutton/sdk/context/package-summary.html", [ [ "Classes", null, [ [ "Event", "com/usebutton/sdk/context/Event.html", null, "" ], [ "Identifiers", "com/usebutton/sdk/context/Identifiers.html", null, "" ], [ "Item", "com/usebutton/sdk/context/Item.html", null, "" ], [ "Journey", "com/usebutton/sdk/context/Journey.html", null, "" ], [ "Location", "com/usebutton/sdk/context/Location.html", null, "" ], [ "MusicArtist", "com/usebutton/sdk/context/MusicArtist.html", null, "" ] ]
+, "" ], [ "Annotations", null, [ [ "Identifiers.IdentifierName", "com/usebutton/sdk/context/Identifiers.IdentifierName.html", null, "" ] ]
+, "" ], [ "Enums", null, [ [ "Journey.Type", "com/usebutton/sdk/context/Journey.Type.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.usebutton.sdk.models", "com/usebutton/sdk/models/package-summary.html", [ [ "Interfaces", null, [ [ "MerchantAction.OnCompleteListener", "com/usebutton/sdk/models/MerchantAction.OnCompleteListener.html", null, "" ], [ "MerchantActionSettings.Builder", "com/usebutton/sdk/models/MerchantActionSettings.Builder.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "AppAction", "com/usebutton/sdk/models/AppAction.html", null, "" ], [ "Footer", "com/usebutton/sdk/models/Footer.html", null, "" ], [ "Header", "com/usebutton/sdk/models/Header.html", null, "" ], [ "Inventory", "com/usebutton/sdk/models/Inventory.html", null, "" ], [ "InventoryGroup", "com/usebutton/sdk/models/InventoryGroup.html", null, "" ], [ "Link", "com/usebutton/sdk/models/Link.html", null, "" ], [ "ListBody", "com/usebutton/sdk/models/ListBody.html", null, "" ], [ "MerchantAction", "com/usebutton/sdk/models/MerchantAction.html", null, "" ], [ "MerchantActionSettings", "com/usebutton/sdk/models/MerchantActionSettings.html", null, "" ], [ "Preview", "com/usebutton/sdk/models/Preview.html", null, "" ], [ "Product", "com/usebutton/sdk/models/Product.html", null, "" ], [ "Text", "com/usebutton/sdk/models/Text.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.usebutton.sdk.util", "com/usebutton/sdk/util/package-summary.html", [ [ "Interfaces", null, [ [ "Arrays.Emitter", "com/usebutton/sdk/util/Arrays.Emitter.html", null, "" ], [ "Arrays.Sink", "com/usebutton/sdk/util/Arrays.Sink.html", null, "" ], [ "Receiver", "com/usebutton/sdk/util/Receiver.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "Arrays", "com/usebutton/sdk/util/Arrays.html", null, "" ], [ "CompatHelpers", "com/usebutton/sdk/util/CompatHelpers.html", null, "" ], [ "CompatHelpers.RippleBackground", "com/usebutton/sdk/util/CompatHelpers.RippleBackground.html", null, "" ], [ "CookiesUtil", "com/usebutton/sdk/util/CookiesUtil.html", null, "" ], [ "LocationProvider", "com/usebutton/sdk/util/LocationProvider.html", null, "" ] ]
+, "" ], [ "Exceptions", null, [ [ "InvalidCookieException", "com/usebutton/sdk/util/InvalidCookieException.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
